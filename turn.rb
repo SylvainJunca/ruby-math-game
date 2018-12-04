ae619e82dcec435860a27f4de0c90017ab29ac23
Sylvain Junca <sylvain.junca@gmail.com>
2018-12-04 01:07:03 +0000

    creates the turn.rb file and object

---
+++ turn.rb
@@ -0,0 +1,15 @@
+class Turn
+  attr_reader :current_player
+
+  def initialize
+    @current_player = rand(0..1)
+  end
+
+  def play_next
+    if @current_player == 0
+      @current_player = 1
+    elsif @current_player == 1
+      @current_player = 0
+    end
+  end
+end
